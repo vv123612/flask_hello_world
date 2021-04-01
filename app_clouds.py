b648d3ef8dc0a8688f06b6eedeba926cc712f90f
vv123612 <78216859+vv123612@users.noreply.github.com>
2021-04-01 15:12:24 +0300

    Update app_clouds.py

--- app_clouds.py
+++ app_clouds.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return f'Hello World! - {datetime.now()'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
