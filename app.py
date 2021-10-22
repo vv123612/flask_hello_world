2e63493abfced97ab3382c8a4206a5bc9e4158c1
Vitaly <v123612@gmail.com>
2021-10-22 21:25:33 +0300

    details info about server

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
 from flask import Flask
 from flask import request
+import sys, os, platform
 app = Flask(__name__)
+
+def get_env():
+    return [("<p>"+key+" = "+value+"<p>") for key,value in dict(os.environ).items()]
+
+
+def info():
+    s='\n'
+
+    return f'''
+    <p>Hello World! </p>
+    <p>IP {request.remote_addr}</p>
+    <p>-----------------------------------------</p>
+    <p>os.name: {os.name}</p>
+    <p>platform.system(): {platform.system()}</p>
+    <p>platform.release(): ', {platform.release()}</p>
+    <p>executable: ', {sys.executable}</p>
+
+    <p>Packages:
+    <p>{s.join(sys.path)}</p>
+    
+    -----------------------------------------
+    <p>os.environ).items() </p>
+    <p>{s.join(get_env())}</p>
+    '''
+
+
 
 @app.route('/')
 def hello_world():
     # return 'Hello World!'
-    return f'''Hello World! \n IP {request.remote_addr}'''
+    # return f'''Hello World! \n IP {request.remote_addr}'''
+    return info()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
--- sys_check.py
+++ sys_check.py
@@ -1,11 +1,46 @@
 import sys, os, platform
+from flask import Flask
+from flask import request
 
-print('-----------------------------------------')
-print('os.name: ',os.name)
-print('platform.system(): '+platform.system())
-print('platform.release(): ', platform.release())
-print('executable: ', sys.executable)
-print('Packages:')
-print('\n'.join(sys.path))
-print('-----------------------------------------')
-[print(key,':',value) for key,value in dict(os.environ).items()]
+
+def info2():
+    print('-----------------------------------------')
+    print('os.name: ',os.name)
+    print('platform.system(): '+platform.system())
+    print('platform.release(): ', platform.release())
+    print('executable: ', sys.executable)
+    print('Packages:')
+    print('\n'.join(sys.path))
+    print('-----------------------------------------')
+    [print(key,':',value) for key,value in dict(os.environ).items()]
+
+
+def info():
+    s='\n'
+
+    return f'''
+    Hello World! 
+    -----------------------------------------')
+    os.name: {os.name}
+    platform.system(): {platform.system()}
+    platform.release(): ', {platform.release()}
+    executable: ', {sys.executable}
+
+    Packages:
+    {s.join(sys.path)}
+    
+    -----------------------------------------
+    os.environ).items() 
+    {s.join([(key+' = '+value) for key,value in dict(os.environ).items()])}
+    '''
+
+        # IP {request.remote_addr}
+
+# def env():
+#     [key+':'+value for key,value in dict(os.environ).items()]
+
+
+
+
+print(info())
+# info2()
