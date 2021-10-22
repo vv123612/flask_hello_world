ff665d6dc85a1f3bf01cfaa19feaf6faa0c4a9f4
Vitaly <v123612@gmail.com>
2021-10-22 13:27:42 +0300

    add IP

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return f'''Hello World! \n IP {request.remote_addr}'''
 
 if __name__ == '__main__':
     app.run(debug=True)
